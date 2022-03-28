8b7ecda3e0618d8e1e7ac5331bd6fca99c8b26e6
Svloex <svetloveexperiment@gmail.com>
2022-03-28 11:15:22 +0300

    sdfkdg

--- routes/api-post-routes.js
+++ routes/api-post-routes.js
@@ -1,20 +1,23 @@
-const express = require("express")
-const { getPost, deletePost, editPost, getPosts, addPost } = require('../controllers/api-post-controller')
+const express = require('express');
+const {
+  getPosts,
+  addPost,
+  getPost, 
+  deletePost,
+  editPost,
+} = require('../controllers/api-post-controller');
 
-const Post = require('../models/post')
-const createPath = require('../helpers/create-path')
+const router = express.Router();
 
-const router = express.Router()
-    //Get All Posts
-router.get('/api/posts', getPosts)
-    //Add New Post
-router.post('/api/post', addPost)
-    //Get Post by ID
-router.post('/api/post/:id', getPost)
-    // Delete Post by ID
-router.delete('/api/post/:id', deletePost)
-    //Update Post by ID 
-router.put('/api/post/:id', editPost)
+// Get All Posts
+router.get('/api/posts', getPosts);
+// Add New Post
+router.post('/api/post/', addPost);
+// Get Post by ID
+router.get('/api/post/:id', getPost);
+// Delete Post by ID
+router.delete('/api/post/:id', deletePost);
+// Update Post by ID
+router.put('/api/post/:id', editPost);
 
-
-module.exports = router
+module.exports = router;
